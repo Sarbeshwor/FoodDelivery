de53885ce12c8af1ea104d45316a8f1b67122ffd
Sarbeshwor Ghimire <98800172+Sarbeshwor@users.noreply.github.com>
2025-06-06 09:54:52 +0600

    halka

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import {assets} from '../../assets/assets'
 
 import Header from '../../components/Header/header'
 import ExploreMenu from '../../components/ExplorMenu/ExploreMenu'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 
 const Home = () => {
+
+  const [category, setCategory]= useState("All");
+
   return (
     <div>
       <Header/>
-      <ExploreMenu/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
+      <FoodDisplay category={category}/>
+
+
+
+
     </div>
   )
 }
