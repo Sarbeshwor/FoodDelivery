77d224f3b4c57bdbd968c8e3a2f931f7769d7888
Sarbeshwor Ghimire <98800172+Sarbeshwor@users.noreply.github.com>
2025-06-06 06:40:04 +0600

    changes have been made

--- frontend/src/components/Header/header.jsx
+++ frontend/src/components/Header/header.jsx
@@ -3,7 +3,16 @@ import './Header.css'
 const header = () => {
   return (
     <div className='header'>
-      
+      <div className="header-contents">
+        <h2>
+          Order from home online
+        </h2>
+        <p>Choose your local restaurant and enjouy the fooood</p>
+        
+      </div>
+      <div className="buttons">
+        <button>View Menu</button>
+      </div>
     </div>
   )
 }
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import './Home.css'
 import {assets} from '../../assets/assets'
 
+import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExplorMenu/ExploreMenu'
+
 const Home = () => {
   return (
     <div>
-      
+      <Header/>
+      <ExploreMenu/>
     </div>
   )
 }
