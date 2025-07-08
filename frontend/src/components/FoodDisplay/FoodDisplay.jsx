6c0ebd9c18bbcd8a9c0a8eacbde030ea1874b908
Sarbeshwor Ghimire <98800172+Sarbeshwor@users.noreply.github.com>
2025-07-09 00:38:00 +0600

    ddd

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -16,7 +16,7 @@ const FoodDisplay = ({ category }) => {
     const fetchFoodItems = async () => {
       try {
         const response = await fetch("https://fooddelivery-uc9i.onrender.com/api/food-items"); 
-        
+        console.log(data);
         const data = await response.json();
         setFoodList(data);
       } catch (error) {
